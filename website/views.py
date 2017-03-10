5d7e0c13b433df3435d987029b99bee61a537a44
P.J. Finlay <pjappdevelopment@gmail.com>
2017-03-10 10:17:22 -0500

    Created language list and language view pages

--- website/views.py
+++ website/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Media
+
 
 def index(request):
-    context = {}
-    return render(request, 'website/index.html', context)
+    media_list = list(Media.objects.all())
+    if not media_list:
+        raise Http404("No Media Found")
+
+    #Get all of the languages
+    language_list = []
+    for media_item in media_list:
+        if media_item.language not in language_list:
+            language_list.append(media_item.language)
+
+    context = {'language_list': language_list}
+    return render(request, 'website/language_list.html', context)
+
+def language(request,language_name):
+    media_list = get_list_or_404(Media, language=language_name)
+    context = {'language_name':language_name,'media_list':media_list}
+    return render(request, 'website/language_view.html', context)
