705781c9d4854a5b51257c67c517900fe3af0c97
P.J. Finlay <pjappdevelopment@gmail.com>
2017-03-21 17:44:38 -0400

    Added all fields to model

--- website/models.py
+++ website/models.py
@@ -4,7 +4,6 @@ from django.db import models
 import re
 
 class Media(models.Model):
-    #The user facing name of the media file
     title = models.CharField(max_length=100)
 
     #Media Server Url ex. mov3.mp4
@@ -13,23 +12,33 @@ class Media(models.Model):
     #This needs to be entered exactly to group by language
     language = models.CharField(max_length=100)
 
-    #Notes
-    notes = models.CharField(max_length=1000, blank=True)
-
     #Course Name ex CS 1110
+    #Comma seperated if multiple courses ex. cs1110, cs2110
     course_name = models.CharField(max_length=100)
 
+    verified_working = models.BooleanField(default=True)
+
+    #This will be displayed to users viewing the media file
+    additional_information = models.CharField(max_length=1000, blank=True)
+
+    enable_subtitles = models.BooleanField(default=True)
+
     #If the code needs to be entered to access the course
-    require_code = models.BooleanField(default=True)
+    require_access_code = models.BooleanField(default=True)
 
     #If the media file should only be allowed to be viewed in the timeframe given below
-    restrict_to_timeframe = models.BooleanField()
+    restrict_to_timeframe = models.BooleanField(default=False)
 
     #Start availability
     start_date = models.DateField(default=datetime.date.today)
 
     #End availability
     end_date = models.DateField(default=datetime.date.today)
+
+    #The day the media was dropped off
+    drop_off_date = models.DateField(default=datetime.date.today)
+
+    requested_by = models.CharField(max_length=100, blank=True)
 
     #Ownership (Personal/Department/Library)
     PERSONAL = 'p'
@@ -46,6 +55,25 @@ class Media(models.Model):
         default=PERSONAL,
     )
 
+    #Format (DVD/VHS/Digital)
+
+    DIGITAL = 'i'
+    DVD = 'd'
+    VHS = 'v'
+    FORMAT_CHOICES = (
+        (DIGITAL,'Library'),
+        (DVD,'Personal'),
+        (VHS,'Department'),
+    )
+    format = models.CharField(
+        max_length=1,
+        choices=FORMAT_CHOICES,
+        default=DIGITAL,
+    )
+
+    #Notes
+    notes = models.CharField(max_length=1000, blank=True)
+
     #Returns true if no code is required or if a valid code is supplied
     def is_valid_code(self, code):
         if not self.require_code:
@@ -53,16 +81,19 @@ class Media(models.Model):
 
         code = code.strip()
 
-        #Get course number ex. CS1110 -> 1110
-        match = re.findall(r'[0-9]+', self.course_name)
-        if len(match) == 1:
-            course_number = match[0]
-        else:
-            course_number = ''
+        course_names = self.course_name.split(',')
 
-        correct_code = self.language.lower().strip() + course_number # ex. french1110
+        for course_name in course_names:
+            #Get course number ex. CS1110 -> 1110
+            match = re.findall(r'[0-9]+', course_name)
+            if len(match) == 1:
+                course_number = match[0]
+            else:
+                course_number = ''
 
-        if correct_code == code:
-            return True
+            correct_code = self.language.lower().strip() + course_number # ex. french1110
+
+            if correct_code == code:
+                return True
 
         return False
