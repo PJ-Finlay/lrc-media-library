e7e274fd37f328c22c208ef59d27baaaf074c9bb
P.J. Finlay <pjappdevelopment@gmail.com>
2017-05-09 16:49:21 -0400

    Added clickable url field to admin interface

--- website/admin.py
+++ website/admin.py
@@ -11,7 +11,14 @@ class MediaAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return True
 
-    list_display = ('title', 'language', 'teacher_username', 'course_name')
-    list_filter = ('language','teacher_username','course_name')  
+    list_display = ('title', 'language', 'teacher_username', 'course_name','get_url_link')
+    list_filter = ('language','teacher_username','course_name')
+
+    #Create clickable link for URL field
+    def get_url_link(self, obj):
+        url_base = 'http://media.lrc.cornell.edu/VOD/'
+        return ('<a href="%s%s" target="_blank">%s</a>' % (url_base, obj.url, obj.url))
+    get_url_link.short_description = 'Name'
+    get_url_link.allow_tags = True
 
 admin.site.register(Media, MediaAdmin)
